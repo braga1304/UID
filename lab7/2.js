15cdfa0f46b73a7bdb9444021761bf052f13e651
braga1304 <120321486+braga1304@users.noreply.github.com>
2022-12-12 00:48:57 +0530

    Add files via upload

---
+++ lab7/2.js
@@ -0,0 +1,33 @@
+function Braga() {
+    var name = document.forms["RegForm"]["Name"].value;
+    var email =
+        document.forms.RegForm.EMail.value;
+    var phone =
+        document.forms.RegForm.Telephone.value;
+    var password =
+        document.forms.RegForm.Password.value;
+    var regEmail=/^\w+([\.-]?\w+)@\w+([\.-]?\w+)(\.\w{2,3})+$/g;  //Javascript reGex for Email Validation.
+    var regPhone=/^\d{10}$/;                                        // Javascript reGex for Phone Number validation.
+    var regName = /\d+$/g;                                    // Javascript reGex for Name validation
+
+    if (name == "" || regName.test(name)) {
+        window.alert("Please enter your name properly.");
+        return false;
+    }
+     
+    if (email == "" || !regEmail.test(email)) {
+        window.alert("Please enter a valid e-mail address.");
+        return false;
+    }
+    
+    if(password.length <6){
+        alert("Password should be atleast 6 character long");
+        return false;
+
+    }
+    if (phone == "" || !regPhone.test(phone)) {
+        alert("Please enter valid phone number.");
+        return false;
+    }
+    return true;
+}
